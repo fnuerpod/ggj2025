a9f02a663cfee83ca5b51496a409c33f4a97b04f
fnuerpod <88922673+fnuerpod@users.noreply.github.com>
2025-01-24 21:47:43 +0000

    feat: pot debounce test

--- Assets/Scripts/Pot.cs
+++ Assets/Scripts/Pot.cs
@@ -1,19 +1,46 @@
 using FMODUnity;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Pot : MonoBehaviour
 {
     public StudioEventEmitter StudioEventEmitter;
+    public BoxCollider Colldier;
 
+    private bool SoundStartedOnLastUpdateCycle = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        StudioEventEmitter.Play();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.name == "Pot")
+            {
+                if (Input.GetMouseButton(0))
+                {
+                    if (SoundStartedOnLastUpdateCycle) return;
+                    StudioEventEmitter.Play();
+                    SoundStartedOnLastUpdateCycle = true;
+                }
+                else
+                {
+                    SoundStartedOnLastUpdateCycle = false;
+                }
+            }
+            else
+            {
+                SoundStartedOnLastUpdateCycle = false;
+            }
+        } else {
+            SoundStartedOnLastUpdateCycle = false;
+        }
     }
 }
