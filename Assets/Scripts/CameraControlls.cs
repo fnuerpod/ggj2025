91e070b853af0fcec6fdb2dcfe3c063ffcdbde10
Taras Voloshyn <157302769+Dartous@users.noreply.github.com>
2025-01-25 14:58:03 +0000

    feat: player movement and camera

---
+++ Assets/Scripts/CameraControlls.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class CameraControlls : MonoBehaviour
+{
+    public Vector3 targetDir;
+    public GameObject player;
+    public Transform playerObject;
+    public Transform orientation;
+    public float rotationSpeed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rotationSpeed = 7f;
+        player = GameObject.FindGameObjectWithTag("Player");
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rotate the camera
+        targetDir = player.transform.position - transform.position;
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDir, Time.deltaTime, 0.0f);
+        transform.rotation = Quaternion.LookRotation(newDirection);
+
+        //rotate the orientation
+        Vector3 viewDir = player.transform.position - new Vector3(transform.position.x, player.transform.position.y, transform.position.z);
+        orientation.forward = viewDir.normalized;
+
+        //rotate the player
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        if (inputDir != Vector3.zero)
+        {
+            playerObject.forward = Vector3.Slerp(playerObject.forward, inputDir.normalized, rotationSpeed*Time.deltaTime);
+        }
+    }
+}
