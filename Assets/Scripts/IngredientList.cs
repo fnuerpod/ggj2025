ecb35b3cda063eabb1166750c7bde8433a5cb0b1
Ryan Shields <ryandennisshields@gmail.com>
2025-01-25 17:55:37 +0000

    initial hint list setup

--- Assets/Scripts/IngredientList.cs
+++ Assets/Scripts/IngredientList.cs
@@ -1,14 +1,19 @@
+using TMPro;
 using UnityEngine;
 
 public class IngredientList : MonoBehaviour
 {
-    public GameObject[] ingredients;
+    [SerializeField] private GameObject[] ingredients;
+    [SerializeField] private TextMeshProUGUI[] hints;
 
     void Start()
     {
         foreach (GameObject ingredient in ingredients)
         {
-            //ingredient.GetComponent<Ingredient>().hints
+            foreach(string hint in ingredient.GetComponent<Ingredient>().hints)
+            {
+                
+            }
         }
     }
 }
