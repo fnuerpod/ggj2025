35473eaae93cb53b9b8c1fb639cfcb7282887894
Ryan Shields <ryandennisshields@gmail.com>
2025-01-25 15:17:27 +0000

    ingredient colour randomisation

--- Assets/Scripts/Ingredient.cs
+++ Assets/Scripts/Ingredient.cs
@@ -1,18 +1,29 @@
+using Unity.VisualScripting;
+using UnityEditor.ShaderGraph;
 using UnityEngine;
 
 public class Ingredient : MonoBehaviour
 {
-    public Material test;
+    private MeshRenderer meshRenderer;
+    public int materialIndex;
+    private Color[] colours =
+    {
+        Color.red,
+        Color.blue,
+        Color.green,
+        Color.yellow,
+        Color.magenta,
+        Color.cyan,
+        Color.white,
+        Color.black,
+        Color.grey,
+        Color.clear
+    };
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        test.color = Color.red;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        meshRenderer = GetComponent<MeshRenderer>(); // Get the MeshRenderer component
+        meshRenderer.materials[materialIndex] = new Material(Shader.Find("Universal Render Pipeline/Lit")); // Create a new material for the ingredient randomisation
+        meshRenderer.materials[materialIndex].color = colours[Random.Range(0, colours.Length)]; // Change the ingredient's colour to one of the random colours
     }
 }
